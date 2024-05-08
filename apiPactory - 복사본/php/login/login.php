a6fa6a668134245dc9eacec6c8013908b16e0936
jaehyuk-lee-0712 <dlwogur0712@gmail.com>
2024-05-08 16:36:33 +0900

    124124

--- apiPactory - 복사본/php/login/login.php
+++ apiPactory - 복사본/php/login/login.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/logintemp.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/logintemp.css">
     <link
         href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet">
@@ -39,12 +39,12 @@
         <div class="wrapper fadeInDown">
             <div id="formContent">
                 <!-- Tabs Titles -->
-                <h2 class=" underlineHover"> 로그인 </h2>
-                <h2 class=" underlineHover"> 회원가입 </h2>
+                <h2 class="active"> 로그인 </h2>
+                <h2 class="inactive underlineHover"> 회원가입 </h2>
 
                 <!-- Icon -->
                 <div class="fadeIn first">
-                    <img src="../assets/img/logo2.png" id="icon" alt="User Icon" />
+                    <img src="../../../assets/img/logo2.png" id="icon" alt="User Icon" />
                 </div>
 
                 <!-- Login Form -->
