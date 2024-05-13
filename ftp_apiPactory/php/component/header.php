3f8499ed7d9aa9fdefa38cb4981301622a1b3c13
jaehyuk-lee-0712 <57936553+jaehyuk-lee-0712@users.noreply.github.com>
2024-05-13 18:29:31 +0900

    124124
    124124124

--- ftp_apiPactory/php/component/header.php
+++ ftp_apiPactory/php/component/header.php
@@ -10,7 +10,7 @@
         </div>
         <div id="menu">
             <ul>
-                <li><a href="#">api 상품22222222222222</a></li>
+                <li><a href="#">api 상품</a></li>
                 <li><a href="#">카테고리</a></li>
                 <li><a href="#">공지사항1</a></li>
             </ul>
@@ -21,6 +21,18 @@
             <i class="fas fa-search"></i>
             <input type="text" placeholder="검색어를 입력하세요" class="search-input">
         </div>
-        <div id="profile"></div>
+<?php
+    if(isset($_SESSION['userID'])) {
+?>            
+        <a href="php/login/logOutAction.php">로그아웃</a>
+<?php
+    }else {
+?>
+        <a href="php/login/login.php">로그인</a>
+        <a href="#">회원가입</a>
+<?php
+    }
+?>
+
     </div>
 </header>
